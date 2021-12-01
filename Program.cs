3567e13765625231a885f44b03c8b9e129c26c62
yaroclav4455 <95227032+yaroclav4455@users.noreply.github.com>
2021-12-01 15:10:25 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
 
+public class Dialogue
+public string Chapter
+{ 
+get
+{
+return chapter; 
+} 
+} 
+publie string Part {
+get 
+{ return part; 
+public List<Speech> Speeches
+{
+get 
+{
+return speeches;
+private string chapter; private string part; private List<Speech> speeches - new List<Speech>();
+public Dialogue(string chapter, string part, List<Speech> speeches)
+chapter = chapter; part = part; speeches = speeches;
+}
+}
+}
