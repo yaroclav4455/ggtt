3dd60f9e5d0c8da006586ada8f960fb6f6e5d0c3
yaroclav4455 <95227032+yaroclav4455@users.noreply.github.com>
2021-12-01 15:28:59 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,24 +1,12 @@
 
-public class Dialogue
-public string Chapter
-{ 
+
+
+
 
 {
-return chapter; 
-}
-jkjkjikj
-} 
-publie string Part {
-get 
-{ return part; 
-public List<Speech> Speeches
-{
-get 
-{
-return speeches;
-private string chapter; private string part; private List<Speech> speeches - new List<Speech>();
-public Dialogue(string chapter, string part, List<Speech> speeches)
-chapter = chapter; part = part; speeches = speeches;
-}
+  static void Main(string[] args)
+  {
+    Console.WriteLine ("Project by Grad Yar");
+  }
 }
 }
